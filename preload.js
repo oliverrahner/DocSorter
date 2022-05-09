18722c793bf0d1058419002d040f36767d6babe3
Oliver Rahner <o.rahner@dke-data.com>
2022-05-09 11:25:04 +0200

    next commit

--- preload.js
+++ preload.js
@@ -2,9 +2,14 @@ const { contextBridge } = require('electron')
 const path = require("path")
 const fs = require("fs")
 const klaw = require("klaw-sync")
+//const chokidar = require("chokidar")
 
-const sourcePath = "Z:/"
-const targetPath = "C:\\Users\\Oliver\\Seafile\\My Library\\Dokumente"
+let applicationConfiguration = {
+    sourcePath: "Z:/",
+    targetPath: "C:\\Users\\Oliver\\Seafile\\My Library\\Dokumente"
+}
+
+//let watcher = chokidar.watch(sourcePath, {persistent: true})
 
 window.addEventListener('DOMContentLoaded', () => {
     const replaceText = (selector, text) => {
@@ -19,17 +24,78 @@ window.addEventListener('DOMContentLoaded', () => {
 })
 
 contextBridge.exposeInMainWorld('myAPI', {
-    listFiles: () => {
+//    addSourceFilesChangedListener: (func) => {
+//        watcher
+//            .on('add', func)
+//            .on('unlink', func);
+//    },
+
+    applicationConfiguration: applicationConfiguration,
+
+    listSourceFiles: () => {
         //const files = await fs.readdir("Z:/")
-        let files = fs.readdirSync(sourcePath, { withFileTypes: true })
+        let files = fs.readdirSync(applicationConfiguration.sourcePath, { withFileTypes: true })
         files = files.filter((value, index) => value.isFile() && value.name.endsWith(".pdf"))
-        files.forEach((value) => value.fullpath = path.join(sourcePath, value.name))
+        files.forEach((value) => value.fullpath = path.join(applicationConfiguration.sourcePath, value.name))
         return files;
     },
 
     listDirectories: () => {
-        let directories = klaw(targetPath, {nofile: true})
-        directories.forEach((value) => value.relativePath = path.relative(targetPath, value.path))
+        let directories = klaw(applicationConfiguration.targetPath, {nofile: true})
+        directories.forEach((value) => value.relativePath = path.relative(applicationConfiguration.targetPath, value.path))
         return directories
+    },
+
+    listTargetFiles: (path) => {
+        let files = fs.readdirSync(path, { withFileTypes: true })
+        files = files.filter((value, index) => value.isFile() && value.name.endsWith(".pdf"))
+        files = files.sort((a, b) => {
+            if (b.name === a.name) return 0;
+            if (b.name < a.name) return -1;
+            return 1;
+        })
+        return files;
+    },
+
+    moveFile: (file, destPath, dateString, name) => {
+        if (typeof(destPath) === "string") {
+            destPath = path.join(applicationConfiguration.targetPath, destPath)
+            if (!fs.existsSync(destPath)) {
+                fs.mkdirSync(destPath)
+            }
+        } else {
+            destPath = destPath.path
+        }
+
+        let targetFilename = dateString + (name !== "" ? " " + name : "") + ".pdf"
+        console.log(targetFilename);
+        let targetPath = path.join(destPath, targetFilename)
+        console.log(targetPath);
+        if (fs.existsSync(targetPath))
+            throw "Target file already exists!"
+        try {
+            fs.copyFileSync(file.fullpath, targetPath)
+        } catch (ex) {
+            throw "Error during file copy: " + ex.message
+        }
+        try {
+            fs.unlinkSync(file.fullpath)
+        } catch (e) {
+            throw "Error during file delete: " + ex.message
+        }
+    },
+
+    deleteFile: (file) => {
+        // does not really delete, only renames to .tobedeleted
+        let targetFilename = file.fullpath + ".tobedeleted"
+        fs.renameSync(file.fullpath, targetFilename)
+    },
+
+    getConfig: () => {
+        return applicationConfiguration
+    },
+
+    setConfig: (config) => {
+        applicationConfiguration = config
     }
 })
--- renderer.js
+++ renderer.js
@@ -1,14 +1,58 @@
 window.addEventListener("DOMContentLoaded", () => {
     reloadFileList()
     reloadDirectoryList()
-    document.getElementById("target-path").addEventListener("input", targetFolderChanged)
+    document.getElementById("target-path").addEventListener("change", targetFolderChanged)
+    document.getElementById("submit").addEventListener("click", submit)
+    document.getElementById("delete-button").addEventListener("click", markForDeletion)
+//    window.myAPI.addSourceFilesChangedListener(reloadFileList);
+
+    const modalTriggers = document.querySelectorAll('.popup-trigger')
+    const modalCloseTrigger = document.querySelector('.popup-modal__close')
+    const bodyBlackout = document.querySelector('.body-blackout')
+    modalTriggers.forEach(trigger => {
+        trigger.addEventListener('click', () => {
+            const { popupTrigger } = trigger.dataset
+            const popupModal = document.querySelector(`[data-popup-modal="${popupTrigger}"]`)
+            popupModal.classList.add('is--visible')
+            bodyBlackout.classList.add('is-blacked-out')
+
+            popupModal.querySelector('.popup-modal__close').addEventListener('click', () => {
+                popupModal.classList.remove('is--visible')
+                bodyBlackout.classList.remove('is-blacked-out')
+            })
+        })
+    })
 })
 
 let files = []
 let fileIndex = -1
 let directories = []
+let selectedDirectory = {}
+let directoryName = ""
 
 const targetFolderChanged = (event) => {
+    let selectedDir = document.querySelector('#target-path').value
+    selectedDirectory = directories.filter(x => x.relativePath === selectedDir)[0]
+    if (selectedDirectory === undefined)
+        directoryName = selectedDir
+    updateExistingFiles()
+}
+
+const updateExistingFiles = () => {
+    let fileList = document.getElementById("existing-documents")
+    fileList.innerHTML = ''
+    if (selectedDirectory === undefined) return;
+    let files = window.myAPI.listTargetFiles(selectedDirectory.path)
+    for (let file of files) {
+        let child = fileList.appendChild(document.createElement("div"))
+        child.innerHTML = file.name
+        child.classList.add("file")
+        child.classList.add("pdf")
+    }
+}
+
+const targetFolderChangedOld = (event) => {
+    return
     let searchValue = event.target.value
     let tokens = searchValue.split(" ")
 
@@ -44,19 +88,25 @@ const targetFolderChanged = (event) => {
 
 const reloadDirectoryList = () => {
     directories = window.myAPI.listDirectories()
+    let datalist = document.getElementById("folderlist")
+    datalist.innerHTML = ''
+    for (let dir of directories) {
+        let option = datalist.appendChild(document.createElement("option"))
+        option.innerHTML = dir.relativePath
+    }
 }
 
 const reloadFileList = () => {
     const currentFilename = files[fileIndex]?.name
-    files = window.myAPI.listFiles()
+    files = window.myAPI.listSourceFiles()
 
     if (fileIndex == -1) {
         fileIndex = 0
     } else {
         // make sure to keep the same file on display when the list has changed
-        const newIndex = files.find(x => x.name === currentFilename).index
+        const newIndex = files.find(x => x.name === currentFilename)?.index
         // if the file has gone, show the next one (meaning index doesn't change)
-        if (newIndex === -1) {
+        if (newIndex === undefined || newIndex === -1) {
             fileIndex--
         } else {
             fileIndex = newIndex
@@ -76,7 +126,7 @@ const showFile = () => {
 }
 
 const clearFileView = () => {
-    // TODO
+    document.getElementById("pdfviewer").setAttribute("src", "./nofiles.html")
 }
 
 const showPreviousFile = () => {
@@ -91,9 +141,53 @@ const showNextFile = () => {
 
 const handleKeyPress = (event) => {
     switch (event.key) {
-        case 'ArrowLeft': if (event.ctrlKey) showPreviousFile(); break;
-        case 'ArrowRight': if (event.ctrlKey) showNextFile(); break;
+        case 'ArrowLeft': if (event.altKey) showPreviousFile(); break;
+        case 'ArrowRight': if (event.altKey) showNextFile(); break;
     }
 }
+
+const submit = () => {
+    try {
+        let result = window.myAPI.moveFile(
+            files[fileIndex],
+            selectedDirectory !== undefined ? selectedDirectory : directoryName,
+            document.getElementById('documentdate').value,
+            document.getElementById('documentname').value
+        )
+        showMessage("File moved successfully.")
+    } catch (ex) {
+            showError("Error: " + ex.message)
+    }
+    reloadFileList()
+}
+
+const markForDeletion = () => {
+    try {
+        let result = window.myAPI.deleteFile(
+            files[fileIndex]
+        )
+        showMessage("File renamed.")
+    } catch (ex) {
+        showError("Error: " + ex.message)
+    }
+    reloadFileList()
+    document.getElementById("target-path").focus()
+}
+
+const showError = (message) => {
+    showMessage(message, "error")
+}
+
+const showMessage = (message, type = "message") => {
+    let messagebox = document.createElement("div")
+    messagebox.classList.add("messagebox")
+    messagebox.classList.add(type)
+    messagebox.innerText = message
+    document.body.appendChild(messagebox)
+    window.setTimeout(() => messagebox.style.opacity = "0", 3000)
+    messagebox.addEventListener("transitionend", () => messagebox.remove())
+}
+
+const showConfigDialog = ()
 
 window.addEventListener('keyup', handleKeyPress, true);
